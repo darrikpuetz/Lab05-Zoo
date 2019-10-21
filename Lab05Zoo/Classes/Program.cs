b1e3e929a8dcd8d3a9b6b8509ea2af0953250403
darrikpuetz <darrikpuetz@gmail.com>
2019-10-21 08:14:32 -0700

    Initial Commit with code from class.

---
+++ Lab05Zoo/Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chris is a class
+            //chriscummings is an intance (a.k.a object)
+            Chris chrisCummings = new Chris("ChrisCummings", true, 999);
+            Chris cheddaChris = new Chris("cheddaChris", true, 0);
+        }
+    }
+}
